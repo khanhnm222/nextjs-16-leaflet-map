7abf9efafa195796361bed3f3bb05c05144a9682
Nguyen Minh Khanh <khanhnm222@gmail.com>
2025-12-28 09:20:00 +0700

    feat: create an error boundary component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProviderWrapper } from "@/components/providers/ThemeProviderWrapper";
+import { ErrorBoundary } from "@/components/common";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProviderWrapper>  
-          {children}
+        <ThemeProviderWrapper>
+          <ErrorBoundary>
+            {children}
+          </ErrorBoundary> 
         </ThemeProviderWrapper>
       </body>
     </html>
